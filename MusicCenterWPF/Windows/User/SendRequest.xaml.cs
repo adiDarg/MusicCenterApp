4f61f1f2b5aac0b6e3ea77495ee9f06a749bfeed
Adi Dargatsky <adidargatsky144@gmail.com>
2025-06-01 15:39:02 +0300

    SendRequest logic implemented

--- MusicCenterWPF/Windows/User/SendRequest.xaml.cs
+++ MusicCenterWPF/Windows/User/SendRequest.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using MusicCenterModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WebApiClient;
 
 namespace MusicCenterWPF.Windows.User
 {
@@ -22,6 +27,60 @@ namespace MusicCenterWPF.Windows.User
         public SendRequest()
         {
             InitializeComponent();
+            this.Loaded += (s, e) => LoadUsers();
+        }
+        private async void LoadUsers()
+        {
+            WebClient<List<MusicCenterModels.User>> webClient = new WebClient<List<MusicCenterModels.User>>
+            {
+                port = 5004,
+                Host = "localhost",
+                Path = "api/User/GetUsers"
+            };
+            List<MusicCenterModels.User> users = await webClient.GetAsync();
+            foreach (var user in users) {
+                ComboBoxItem item = new ComboBoxItem { 
+                    Content = user.Name,
+                    Tag = user.Id
+                };
+                recieverInput.Items.Add(item);
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string recieverID = (recieverInput.SelectedItem as ComboBoxItem).Tag as string;
+            if (recieverID == null)
+            {
+                MessageBox.Show("Please select a reciever.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            string title = titleInput.Text;
+            string description = descriptionInput.Text;
+            Request request = new Request { 
+                Title = title,
+                Description = description,
+                Reciever = new MusicCenterModels.User { Id = recieverID },
+                Sender = new MusicCenterModels.User { Id = SessionManager.UserID },
+                IsApproved = false,
+                IsSeen = false,
+                RequestType = (typeInput.SelectedItem as ComboBoxItem).Content as string
+            };
+            if (request.RequestType == null)
+            {
+                MessageBox.Show("Please enter a request type.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            WebClient<Request> client = new WebClient<Request>();
+            client.port = 5004;
+            client.Host = "localhost";
+            client.Path = "api/User/SendRequest";
+            client.AddParams("request", JsonSerializer.Serialize(request));
+            bool success = await client.PostAsync(request);
+            MessageBox.Show(success? "Request sent.":"Request failed to send.",
+                success? "Success":"Error",
+                MessageBoxButton.OK,
+                success? MessageBoxImage.Information: MessageBoxImage.Error);
         }
     }
 }
