7a29c0986dc8173453364fced3be46803ee0b814
Adi Dargatsky <adidargatsky144@gmail.com>
2025-06-02 19:47:31 +0300

    viewTeachers with logic

--- MusicCenterWPF/Windows/Registree/ViewTeachers.xaml.cs
+++ MusicCenterWPF/Windows/Registree/ViewTeachers.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicCenterWPF.Windows.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WebApiClient;
+using UserModel = MusicCenterModels.User;
 
 namespace MusicCenterWPF.Windows.Registree
 {
@@ -22,11 +25,42 @@ namespace MusicCenterWPF.Windows.Registree
         public ViewTeachers()
         {
             InitializeComponent();
+            this.Loaded += async (s, e) => {
+                WebClient<List<UserModel>> webClient = new WebClient<List<UserModel>>{
+                    port = 5004,
+                    Host = "localhost",
+                    Path = "api/Registree/ViewAllTeachers"
+                };
+                List<UserModel> users = await webClient.GetAsync();
+                LoadProfiles(users); 
+            };
         }
-
-        private void NavBar_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        private void LoadProfiles(List<UserModel> users)
         {
+            int row = 0;
+            int column = 0;
+            profileCardsGrid.RowDefinitions.Add(new RowDefinition());
+            foreach (var user in users)
+            {
+                ProfileCard card = new ProfileCard();
+                card.Profile = user;
 
+                Border border = new Border { 
+                    Style = this.FindResource("CellBorderStyle") as Style,
+                    Child = card
+                };
+                Grid.SetRow(border, row);
+                Grid.SetColumn(border, column);
+                profileCardsGrid.Children.Add(border);
+
+                column++;
+                if (column == 4)
+                {
+                    row++;
+                    column = 0;
+                    profileCardsGrid.RowDefinitions.Add(new RowDefinition());
+                }
+            }
         }
     }
 }
