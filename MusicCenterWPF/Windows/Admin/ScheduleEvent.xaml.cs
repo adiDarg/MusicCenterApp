1dc1d52bca448e39d0001f1849310b685c4a068a
Adi Dargatsky <adidargatsky144@gmail.com>
2025-06-03 20:00:02 +0300

    schedule event - need to make placeholders invisible with combobox

---
+++ MusicCenterWPF/Windows/Admin/ScheduleEvent.xaml.cs
@@ -0,0 +1,174 @@
+﻿using MusicCenterModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WebApiClient;
+
+namespace MusicCenterWPF.Windows.Admin
+{
+    /// <summary>
+    /// Interaction logic for ScheduleEvent.xaml
+    /// </summary>
+    public partial class ScheduleEvent : Window
+    {
+        public ScheduleEvent()
+        {
+            InitializeComponent();
+            this.Loaded += (s, e) =>
+            {
+                LoadGroups();
+                LoadStudents();
+            };
+        }
+        private async void LoadGroups()
+        {
+            WebClient<List<Group>> webClient = new WebClient<List<Group>>
+            {
+                port = 5004,
+                Host = "localhost",
+                Path = "api/Admin/GetGroups"
+            };
+            List<Group> groups = await webClient.GetAsync();
+            foreach (var group in groups)
+            {
+                ComboBoxItem item = new ComboBoxItem
+                {
+                    Content = group.Name,
+                    Tag = group.Id
+                };
+                groupComboBox.Items.Add(item);
+            }
+        }
+        private async void LoadStudents()
+        {
+            WebClient<List<MusicCenterModels.Registree>> webClient = new WebClient<List<MusicCenterModels.Registree>>
+            {
+                port = 5004,
+                Host = "localhost",
+                Path = "api/Admin/GetRegistrees"
+            };
+            List<MusicCenterModels.Registree> registrees = await webClient.GetAsync();
+            foreach (var registree in registrees)
+            {
+                ComboBoxItem item = new ComboBoxItem
+                {
+                    Content = registree.Name,
+                    Tag = registree.Id
+                };
+                studnetComboBox.Items.Add(item);
+            }
+        }
+        private void actionChoiceBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (actionChoiceBox.SelectedItem != null) {
+                if ((actionChoiceBox.SelectedItem as ComboBoxItem).Content.Equals("Lesson"))
+                {
+                    LessonGrid.Visibility = Visibility.Visible;
+                    MeetingGrid.Visibility = Visibility.Hidden;
+                }
+                else if ((actionChoiceBox.SelectedItem as ComboBoxItem).Content.Equals("Meeting"))
+                {
+                    LessonGrid.Visibility = Visibility.Hidden;
+                    MeetingGrid.Visibility = Visibility.Visible;
+                }
+            }
+        }
+        private async void studentComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (studnetComboBox.SelectedItem == null)
+            {
+                return;
+            }
+            WebClient<List<MusicCenterModels.Teacher>> webClient = new WebClient<List<MusicCenterModels.Teacher>>
+            {
+                port = 5004,
+                Host = "localhost",
+                Path = "api/Admin/GetTeachersByRegistree"
+            };
+            webClient.AddParams("registreeID", (studnetComboBox.SelectedItem as ComboBoxItem).Tag as string);
+            List<MusicCenterModels.Teacher> teachers = await webClient.GetAsync();
+            LoadTeachers(teachers);
+        }
+        private void LoadTeachers(List<MusicCenterModels.Teacher> teachers)
+        {
+            teacherComboBox.Items.Clear();
+            foreach (var teacher in teachers)
+            {
+                ComboBoxItem teacherComboBoxItem = new ComboBoxItem
+                {
+                    Content = teacher.Name,
+                    Tag = teacher.Id
+                };
+                teacherComboBox.Items.Add(teacherComboBoxItem);
+            }
+        }
+
+        private async void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DateTime dateTime = datePicker.DisplayDate;
+                string date = dateTime.ToString("s");
+                string room = roomTextBox.Text;
+                if (((actionChoiceBox.SelectedItem as ComboBoxItem).Content as string).Equals("Lesson"))
+                {
+                    string studnetID = (studnetComboBox.SelectedItem as ComboBoxItem).Tag as string;
+                    string teacherID = (teacherComboBox.SelectedItem as ComboBoxItem).Tag as string;
+                    Lesson lesson = new Lesson {
+                        Date = date,
+                        Room = room,
+                        Student = new MusicCenterModels.Registree { Id = studnetID },
+                        Teacher = new MusicCenterModels.Teacher { Id = teacherID }
+                    };
+                    WebClient<Lesson> lessonWebClient = new WebClient<Lesson> {
+                        port = 5004,
+                        Host = "localhost",
+                        Path = "api/Admin/CreateLesson"
+                    };
+                    lessonWebClient.AddParams("lesson",JsonSerializer.Serialize(lesson));
+                    bool success = await lessonWebClient.PostAsync(lesson);
+                    MessageBox.Show(success? "Lesson posted.": "Failed to post lesson.",
+                        success? "Success":"Error",
+                        MessageBoxButton.OK,
+                        success? MessageBoxImage.Information : MessageBoxImage.Error);
+                }
+                else if (((actionChoiceBox.SelectedItem as ComboBoxItem).Content as string).Equals("Meeting"))
+                {
+                    string groupID = (groupComboBox.SelectedItem as ComboBoxItem).Tag as string;
+                    Meeting meeting = new Meeting
+                    {
+                        Date = date,
+                        Room = room,
+                        Group = new Group { Id = groupID }
+                    };
+                    WebClient<Meeting> meetingWebClient = new WebClient<Meeting>
+                    {
+                        port = 5004,
+                        Host = "localhost",
+                        Path = "api/Admin/CreateMeeting"
+                    };
+                    meetingWebClient.AddParams("meeting", JsonSerializer.Serialize(meeting));
+                    bool success = await meetingWebClient.PostAsync(meeting);
+                    MessageBox.Show(success ? "Meeting posted." : "Failed to post meeting.",
+                        success ? "Success" : "Error",
+                        MessageBoxButton.OK,
+                        success ? MessageBoxImage.Information : MessageBoxImage.Error);
+                }
+            }
+            catch {
+                MessageBox.Show("Please fill all fields","Error",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+    }
+}
